7864a7fffc8da2cd59c55505175a43486fdd8ac7
Beata Dębińska <bea.debinska@gmail.com>
2020-10-30 20:23:13 +0100

    Add header, footer and about us and contact us pages

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const homeStartingContent = "Home starting content goes here.";
+const aboutContent = "About content goes here.";
+const contactContent = "Contact content goes here.";
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home.ejs", {homeStartingContent: homeStartingContent});
+});
+
+app.get("/about", function(req, res) {
+    res.render("about.ejs", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res) {
+    res.render("contact.ejs", {contactContent: homeStartingContent});
 });
 
 app.listen(3000, function() {
