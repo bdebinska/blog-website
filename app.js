7b77f14858baf4d76007c6ffe0f353a97fe4a397
Beata Dębińska <bea.debinska@gmail.com>
2020-10-31 11:07:01 +0100

    Add compose page

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+// web page content
+
 const homeStartingContent = "Home starting content goes here.";
 const aboutContent = "About content goes here.";
 const contactContent = "Contact content goes here.";
+
+const posts = [];
 
 const app = express();
 
@@ -15,16 +19,38 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// main page
+
 app.get("/", function(req, res) {
     res.render("home.ejs", {homeStartingContent: homeStartingContent});
 });
+
+// about us page
 
 app.get("/about", function(req, res) {
     res.render("about.ejs", {aboutContent: aboutContent});
 });
 
+// contact page
+
 app.get("/contact", function(req, res) {
     res.render("contact.ejs", {contactContent: homeStartingContent});
+});
+
+// compose page
+
+app.get("/compose", function(req, res) {
+    res.render("compose.ejs");
+});
+
+app.post("/compose", function(req, res) {
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    }
+
+    posts.push(post);
+    res.redirect("/");
 });
 
 app.listen(3000, function() {
